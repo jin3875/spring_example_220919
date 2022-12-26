040bdc618bdbe9f488c13f41a1eb3ce7067d7f10
test <test@test.com>
2022-12-26 18:40:46 +0900

    221226

--- src/main/java/com/example/lesson03/bo/ReviewBO.java
+++ src/main/java/com/example/lesson03/bo/ReviewBO.java
@@ -15,5 +15,18 @@ public class ReviewBO {
 	public Review getReview(int id) { // BO는 id를 필수로 필요로 함 -> int
 		return reviewDAO.selectReview(id);
 	}
+	
+	// input : Review 객체
+	// output: int(인서트가 성공된 행 수)
+	public int addReview(Review review) {
+		return reviewDAO.insertReview(review);
+	}
+	
+	// addReviewAsField(4, "콤비네이션R", "신바다", 5.0, "역시 맛있다!")
+	public int addReviewAsField(
+			int storeId, String menu, String userName, Double point, String review // null 허용 X -> double, null 허용 -> Double
+	) {
+		return reviewDAO.insertReviewAsField(storeId, menu, userName, point, review);
+	}
 
 }
