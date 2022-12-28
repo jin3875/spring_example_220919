2eda55fe4f4793709c4281d4d265219ee96a799d
test <test@test.com>
2022-12-28 23:14:17 +0900

    221228 과제

--- src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
+++ src/main/java/com/example/lesson04/Lesson04Ex02Controller.java
@@ -1,16 +1,21 @@
 package com.example.lesson04;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.lesson04.bo.StudentBO;
 import com.example.lesson04.model.Student;
 
 @RequestMapping("/lesson04/ex02")
 @Controller
 public class Lesson04Ex02Controller {
+	
+	@Autowired
+	private StudentBO studentBO;
 	
 	// 학생 추가 화면
 	// http://localhost/lesson04/ex02/add_student_view
@@ -24,6 +29,7 @@ public class Lesson04Ex02Controller {
 			@ModelAttribute Student student // name(request param)과 필드명이 일치하는 것이 담긴다. (Model 1개 -> 어노테이션 생략 가능)
 	) {
 		// DB insert -> 과제
+		studentBO.addStudent(student);
 		
 		// DB select (방금 가입된 사람)
 		
